97d2ebaf26291f1f5fba7739553d82a857222fac
Abhishek Bhati <bhatiabhishek597@example.com>
2025-10-05 16:09:47 +0530

    Your commit message describing the changes

--- server/server.js
+++ server/server.js
@@ -14,6 +14,7 @@ app.use(cors());
 app.use(express.json());
 
 // Routes
+app.get('/',(req,res)=> res.send('server is live'));
 app.use("/api/auth", authRoutes);
 app.use("/api/resume", resumeRoutes);
 app.use("/api/job", jobRoutes);
